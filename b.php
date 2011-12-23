4181a8bbf2edcb7d45310179d3272dd2c09785c6
lawl <root@s3cr3t.de>
2011-12-23 20:29:08 +0100

    added comment feature

--- b.php
+++ b.php
@@ -10,14 +10,18 @@ define('T_HEADER','template_header');
 define('T_FOOTER','template_footer');
 define('T_POST','template_post');
 define('T_ADMIN','template_addpost');
+define('T_CMNTFRM','template_commentform');
+define('T_CMNT','template_comment');
+define('T_FAIL','template_fail');
 define('D_POSTTITLE','posttitle');
 define('D_POSTCONTENT','postcontent');
 define('D_POSTDATE','postdate');
+define('D_NAME','name');
+define('D_COMMENT','comment');
 define('D_POSTID','postid');
 //INSTALL STUFF
 if(get_kvp(B,'firstuse')===false){
-	echo 'Configuring b.php... This might take a while.';
-	if(!record_exists(''))if(!mkdir(DATAPATH))die('Can not create database. Create directory "'.DATAPATH.'" and make it writeable.');
+	if(!record_exists(''))if(!mkdir(DATAPATH))die('Can\'t create database. Create directory "'.DATAPATH.'" and make it writeable.');
 	create_record(B);
 	create_index(D_POSTDATE,D_POSTDATE);
 	set_kvp(B,T_HEADER, <<< 'EOD'
@@ -36,7 +40,7 @@ EOD
 	);
 	set_kvp(B,T_POST, <<< 'EOD'
 	<hr />
-	<b>{{POSTTITLE}}</b> <i>{{POSTDATE}}</i> <a href="?edit={{POSTID}}">edit</a> <a href="?delete={{POSTID}}">delete</a><br />
+	<a href="?a={{POSTID}}"><b>{{POSTTITLE}}</b></a> <i>{{POSTDATE}}</i> <a href="?edit={{POSTID}}">edit</a> <a href="?delete={{POSTID}}">delete</a><br />
 	{{POSTCONTENT}}
 	<hr />
 EOD
@@ -44,11 +48,37 @@ EOD
 	set_kvp(B,T_ADMIN, <<< 'EOD'
 	<div>
 	<form action="" method="post">
-	<input name="posttitle" type="text" value="{{POSTTITLE}}"><br />
-	<textarea name="postcontent" rows="10" cols="70">{{POSTCONTENT}}</textarea><br />
-	<input name="postid" type="hidden" value="{{POSTID}}" />
-	<input name="submitpost" type="submit" value="commit" />
+		Title <input name="posttitle" type="text" value="{{POSTTITLE}}"><br />
+		Post<br />
+		<textarea name="postcontent" rows="10" cols="70">{{POSTCONTENT}}</textarea><br />
+		<input name="postid" type="hidden" value="{{POSTID}}" />
+		<input name="submitpost" type="submit" value="commit" />
 	</form>
+EOD
+	);
+	set_kvp(B,T_CMNTFRM, <<< 'EOD'
+	<div>
+	<form action="" method="post">
+		Nick <input name="name" type="text" value="Anonymous"><br />
+		Comment<br />
+		<textarea name="comment" rows="10" cols="70"></textarea><br />
+		<input name="postid" type="hidden" value="{{POSTID}}" />
+		<input name="submitcmnt" type="submit" value="commit" />
+	</form>
+EOD
+	);
+	set_kvp(B,T_CMNT, <<< 'EOD'
+	<hr />
+	<div>
+		<div>{{NAME}}</div>
+		<div>{{COMMENT}}</div>
+		<a href="?dc&postid={{POSTID}}&cid={{CID}}">delete</a>
+	</div>
+	<hr />
+EOD
+	);
+	set_kvp(B,T_FAIL, <<< 'EOD'
+	SOMETHING FAILED! (probably you, now go back and figure it out)
 EOD
 	);
 	set_kvp(B,'firstuse',1);
@@ -66,7 +96,11 @@ function get_kvp($r,$k){
 	$p=DATAPATH.sanitize_key($r).'/'.sanitize_key($k);
 	return file_exists($p)?file_get_contents($p):false;
 }
+function delete_kvp($r,$kvp){
+	unlink(DATAPATH.sanitize_key($r).'/'.sanitize_key($kvp));
+}
 function record_exists($p){
+	$p=sanitize_key($p);
 	return file_exists(DATAPATH.$p)&&is_dir(DATAPATH.$p);
 }
 function record_delete($r){
@@ -81,6 +115,10 @@ function record_delete($r){
 		closedir($h);
 		rmdir(DATAPATH.$r);
 	}
+}
+function get_keys($r){
+	$s=scandir(DATAPATH.$r);
+	return array_values(array_filter($s,function($v){return $v!='.'&&$v!='..';}));
 }
 function sanitize_key($k){
 	return preg_replace('/[^A-Za-z0-9_]/','',$k);
@@ -114,6 +152,12 @@ function tpl(){
 function tpl_set($t,$w,$r){
 	return str_replace('{{'.$w.'}}',$r,$t);	
 }
+function fail(){
+	echo tpl(T_HEADER,'SITENAME',SITENAME);
+	echo tpl(T_FAIL);
+	echo tpl(T_FOOTER);
+	die();
+}
 //ADMIN STUFF
 if(isset($_POST['submitpost'])){//POST ACTIONS
 	$r=0;
@@ -121,8 +165,7 @@ if(isset($_POST['submitpost'])){//POST ACTIONS
 		$r=create_record(uniqid());
 		set_kvp($r,D_POSTDATE,time());
 	}else{
-		$r=sanitize_key($_POST[D_POSTID]);
-		if(!record_exists($r))die("dum");
+		if(!record_exists($_POST[D_POSTID]))fail();
 	}
 	set_kvp($r,D_POSTTITLE,$_POST[D_POSTTITLE]);
 	set_kvp($r,D_POSTCONTENT,$_POST[D_POSTCONTENT]);
@@ -132,7 +175,25 @@ if(isset($_GET['delete'])){
 	record_delete($_GET['delete']);
 	create_index(D_POSTDATE,D_POSTDATE);
 }
+if(isset($_GET['dc'])){
+	$cfl=$_GET['postid'].D_COMMENT;
+	if(!record_exists($cfl))fail();
+	delete_kvp($cfl,$_GET['cid'].'_'.D_NAME);
+	delete_kvp($cfl,$_GET['cid'].'_'.D_COMMENT);
+	delete_kvp($cfl,$_GET['cid'].'_'.D_POSTDATE);
+}
 if(isset($_GET['rbindex']))create_index(D_POSTDATE,D_POSTDATE);
+//ADD COMMENT
+if(isset($_POST['submitcmnt'])){
+	if(empty($_POST[D_COMMENT])||empty($_POST[D_NAME]))fail();
+	$r=$_POST[D_POSTID].D_COMMENT;
+	if(!record_exists($_POST[D_POSTID]))fail();
+	if(!record_exists($r))create_record($r);
+	$u=uniqid();
+	set_kvp($r,$u.'_'.D_POSTDATE,time());
+	set_kvp($r,$u.'_'.D_NAME,$_POST[D_NAME]);
+	set_kvp($r,$u.'_'.D_COMMENT,$_POST[D_COMMENT]);
+}
 //BB STUFF
 function parsebb($t){
 	$t = preg_replace('/\[b\](.+?)\[\/b\]/is','<b>\1<\/b>',$t);
@@ -147,18 +208,33 @@ function parsebb($t){
 //BLOGGY STUFF
 echo tpl(T_HEADER,'SITENAME',SITENAME);
 if(isset($_GET['edit'])){
-	$id=sanitize_key($_GET['edit']);
-	if(!record_exists($id))die("dum");
-	$tpl=tpl(T_ADMIN,'POSTTITLE',get_kvp($id,D_POSTTITLE),'POSTCONTENT',get_kvp($id,D_POSTCONTENT),'POSTID',$id);
+	if(!record_exists($_GET['edit']))fail();
+	$tpl=tpl(T_ADMIN,'POSTTITLE',get_kvp($_GET['edit'],D_POSTTITLE),'POSTCONTENT',get_kvp($_GET['edit'],D_POSTCONTENT),'POSTID',$_GET['edit']);
 }else{
 	$tpl=tpl(T_ADMIN,'POSTTITLE','','POSTCONTENT','','POSTID','');
 }
 echo tpl_set($tpl,'SITENAME',SITENAME);
-
 $p=get_index(D_POSTDATE);
+$o=0;
+if(isset($_GET['a']) && record_exists($_GET['a'])){
+	$o=1;
+	$p=array(array(VALUE => get_kvp($_GET['a'],D_POSTDATE), KEY => $_GET['a']));
+}
 uasort($p,function($a,$b){if($a[VALUE]==$b[VALUE])return 0;return $a[VALUE]<$b[VALUE];});
 foreach($p as $m){
 	echo tpl(T_POST,'POSTID',$m[KEY],'POSTTITLE',get_kvp($m[KEY],D_POSTTITLE),'POSTCONTENT',parsebb(nl2br(get_kvp($m[KEY],D_POSTCONTENT))),'POSTDATE',date('d.m.Y',$m[VALUE]));
+	if($o){
+		$r=$m[KEY].D_COMMENT;
+		if(record_exists($r)){
+			$c=get_keys($r);
+			$c=array_unique(array_map(function($e){$e=explode('_',$e);return $e[0];},$c));
+			foreach($c as $d){
+				echo tpl(T_CMNT,'NAME',get_kvp($r,$d.'_'.D_NAME),'COMMENT',get_kvp($r,$d.'_'.D_COMMENT),'POSTID',$m[KEY],'CID',$d);
+			}
+		}
+		echo tpl(T_CMNTFRM,'POSTID',$m[KEY]);
+		break;
+	}
 }
 echo tpl(T_FOOTER);
 echo memory_get_peak_usage()/1024;
